206e7b48d93508acc11e4cdee3f8eda88f56f5d2
Tyler Smith <tylerlwsmith@gmail.com>
2024-07-12 17:22:28 -0700

    Replace $fillable with empty $guarded on User

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,15 +12,11 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are not mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
